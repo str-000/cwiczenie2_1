97eee0cffec1f3cf542479ed9bbea02ccba79c60
str-000 <strama.lukasz54@gmail.com>
2021-11-19 12:00:22 +0100

    base

---
+++ app/src/main/java/edu/pl/zse/napisy/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.pl.zse.napisy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
